176ef3928174af76fb7e59c8303abae4cebbf4c0
Matheus Neder <matheusneder@gmail.com>
2018-08-20 10:55:08 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# otc-validations
+# Otc.Validations
+[![Build Status](https://travis-ci.org/OleConsignado/otc-validations.svg?branch=master)](https://travis-ci.org/OleConsignado/otc-validations)
+
 Provide convenient way to perform annotations based model validation.
---
+++ Source/Otc.Validations.Helpers/ValidationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Otc.ComponentModel.DataAnnotations;
+using System.Linq;
+using Otc.DomainBase.Exceptions;
+
+namespace Otc.Validations.Helpers
+{
+    public class ValidationHelper
+    {
+        /// <summary>
+        /// Valida as propriedades do modelo informado e dispara uma ValidationException caso não seja valido.
+        /// </summary>
+        /// <typeparam name="T">Tipo do modelo que sera validado</typeparam>
+        /// <param name="model">Instancia do modelo que sera validado</param>
+        /// <exception cref="ModelValidationException" />
+        public static void ThrowValidationExceptionIfNotValid<T>(T model)
+        {
+            if (!ModelValidator.TryValidate(model, out IEnumerable<ValidationResult> errors))
+            {
+                throw new ModelValidationException(
+                    errors.Select(e => 
+                        new ModelValidationError(e.ErrorKey, e.ErrorMessage)).ToArray());
+            }
+        }
+    }
+}
